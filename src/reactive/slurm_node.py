d174d5d98347e82a3640a4836be66bf737366962
Niclas Ekblad <nickeekblad@hotmail.com>
2018-08-20 12:10:52 +0200

    Added enpoint to slurm-cluster flag

--- src/reactive/slurm_node.py
+++ src/reactive/slurm_node.py
@@ -27,7 +27,7 @@ import charms.reactive.flags as flags
 
 
 
-flags.register_trigger(when='slurm-cluster.active.changed',
+flags.register_trigger(when='endpoint.slurm-cluster.active.changed',
                        clear_flag='slurm-node.configured')
 
 
